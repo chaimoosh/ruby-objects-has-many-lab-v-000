bdd5203b370cf53db767b28a0bcb7302348d1306
Chaim Reisman <crreisman@gmail.com>
2018-02-09 03:29:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,26 @@
 require'pry'
 class Artist 
   attr_accessor :name 
+<<<<<<< HEAD
   @@song_count = 0
+=======
+  
+>>>>>>> 7a8e13c11072c08df30c2ca3d7b2c0ad599f5098
   def initialize(name)
     @name = name 
     @songs = []
   end 
+<<<<<<< HEAD
    
+=======
+  
+>>>>>>> 7a8e13c11072c08df30c2ca3d7b2c0ad599f5098
   def songs 
     @songs 
   end
   
   def add_song(name)
+<<<<<<< HEAD
     @songs << name
     name.artist = self
     @@song_count += 1
@@ -26,5 +35,10 @@ class Artist
   
   def self.song_count
     @@song_count
+=======
+    binding.pry
+    name = Song.new(name)
+    @songs << name
+>>>>>>> 7a8e13c11072c08df30c2ca3d7b2c0ad599f5098
   end
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song 
+<<<<<<< HEAD
   attr_accessor :name, :artist 
 
   def initialize(name)
@@ -8,4 +9,11 @@ class Song
   def artist_name
     artist.name if artist 
   end
+=======
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end 
+>>>>>>> 7a8e13c11072c08df30c2ca3d7b2c0ad599f5098
 end 
